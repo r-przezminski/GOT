99d3b91eea840c1e0774b11a120ad4be99021144
r-przezminski <r.przezminski@gmail.com>
2017-11-08 21:45:12 +0100

    added vuex package

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue         from 'vue'
 import App         from './App'
 import router      from './router'
 import VueResource from 'vue-resource'
+import Vuex        from 'vuex'
 
+Vue.use(Vuex);
 Vue.use(VueResource);
 Vue.http.options.root = 'https://api.got.show/api/';
 Vue.config.productionTip = false;
