0753265e727ab737988ab90e8a258235a13c09bd
r-przezminski <r.przezminski@gmail.com>
2018-01-12 20:04:27 +0100

    'commit'

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,6 +1,29 @@
 <template>
 	<div>
 		<h1>Home</h1>
+		<img src="https://api.got.show/misc/images/characters/Jon_Snow.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Eddard_Stark.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Robb_Stark.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Arya_Stark.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Robert_Baratheon.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Aegon_I_Targaryen.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Tywin_Lannister.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Tyrion_Lannister.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Barristan_Selmy.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Bronn.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Cersei_Lannister.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Daario_Naharis.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Daemon_Targaryen.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Davos_Seaworth.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Drogo.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Ellaria_Sand.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Jaime_Lannister.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Jaqen_H'ghar.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Mag_Mar_Tun_Doh_Weg.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Margaery_Tyrell.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Melisandre.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Sandor_Clegane.jpeg" alt="">
+		<img src="https://api.got.show/misc/images/characters/Ygritte.jpeg" alt="">
 	</div>
 </template>
 
