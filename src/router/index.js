0c679ce2f5f0b7313c930ccd71066a560794a12f
r-przezminski <32273937+r-przezminski@users.noreply.github.com>
2020-03-09 12:51:10 +0100

    commit from sandbox
    Test commit from sandbox

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,48 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
-function load(component) {
-	return () => System.import(`@/components/${component}`)
-}
+// function load(component) {
+// 	return () => System.import(`@/components/${component}`)
+// }
+
+const load = component => {
+  return () => import(`@/components/${component}`);
+};
 
 export default new Router({
-	mode: 'history',
-	routes: [
-		{
-			path: '/',
-			name: 'Home',
-			component: load('Home/Home')
-		},
-		{
-			path: '/characters',
-			name: 'Characters',
-			component: load('Characters/Characters')
-		},
-		{
-			path: '/cities',
-			name: 'Cities',
-			component: load('Cities/Cities')
-		},
-		{
-			path: '/houses',
-			name: 'Houses',
-			component: load('Houses/Houses')
-		},
-		{
-			path: '/episodes',
-			name: 'Episodes',
-			component: load('Episodes/Episodes')
-		}
-	]
-})
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: load("Home/Home")
+      // component: import("@/componets/Home/Home")
+    },
+    {
+      path: "/characters",
+      name: "Characters",
+      component: load("Characters/Characters")
+      // component: import("@/componets/Characters/Characters")
+    },
+    {
+      path: "/cities",
+      name: "Cities",
+      component: load("Cities/Cities")
+      // component: import("@/componets/Cities/Cities")
+    },
+    {
+      path: "/houses",
+      name: "Houses",
+      component: load("Houses/Houses")
+      // component: import("@/componets/Houses/Houses")
+    },
+    {
+      path: "/episodes",
+      name: "Episodes",
+      component: load("Episodes/Episodes")
+      // component: import("@/componets/Episodes/Episodes")
+    }
+  ]
+});
